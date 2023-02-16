494942eca3b9082d799d0dbf4d2105dbb0615b1b
Chris Onoja Idoko <chrisidoko0017@gmail.com>
2023-02-16 13:43:24 +0100

    New Slider Completed

--- src/Components/buttomSlider/slider.js
+++ src/Components/buttomSlider/slider.js
@@ -1,33 +1,72 @@
 import "./slider.scss";
-import { IoIosArrowDropright } from "react-icons/io";
+import { IoIosArrowDropright, IoIosArrowDropleft } from "react-icons/io";
 import { buttomData } from "./sliderData";
-import { motion } from "framer-motion";
-import { useState, useRef, useEffect } from "react";
+import { useRef } from "react";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
+// import required modules
+import { Keyboard, Navigation } from "swiper";
 
 function Slider() {
-  const [width, setWidth] = useState(0);
-  const sl_slider = useRef();
-  useEffect(() => {
-    setWidth(sl_slider.current.scrollWidth - sl_slider.current.offsetWidth);
-  }, []);
-
+  const swiperNavPrevRef = useRef(null);
+  const swiperNavNextRef = useRef(null);
   return (
     <>
-      <div className="sl_grp1">
-        <span className="sl_header">Games On Sale</span>
-        <span className="sl_arrow2">
-          <IoIosArrowDropright />
-        </span>
-      </div>
-      <motion.div ref={sl_slider} className="sl_slider">
-        <motion.div
-          drag="x"
-          dragConstraints={{ right: 0, left: -width }}
-          className="sl_flex"
-        >
-          {buttomData.map((val, key) => {
-            return (
-              <motion.div key={key} className="items_flex">
+      <Swiper
+        keyboard={{
+          enabled: true,
+        }}
+        navigation={{
+          nextEl: swiperNavNextRef.current,
+          prevEl: swiperNavPrevRef.current,
+        }}
+        breakpoints={{
+          340: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+          },
+          640: {
+            slidesPerView: 3,
+            spaceBetween: 10,
+          },
+          768: {
+            slidesPerView: 4,
+            spaceBetween: 15,
+          },
+          1024: {
+            slidesPerView: 5,
+            spaceBetween: 20,
+          },
+        }}
+        modules={[Keyboard, Navigation]}
+        className="mySwiper"
+        onInit={(swiper) => {
+          swiper.params.navigation.prevEl = swiperNavPrevRef.current;
+          swiper.params.navigation.nextEl = swiperNavNextRef.current;
+          swiper.navigation.init();
+          swiper.navigation.update();
+        }}
+      >
+        <div className="sl_header">Games On Sale</div>
+
+        <div className="Navgroup">
+          <div className="swiperNavNext" ref={swiperNavNextRef}>
+            <IoIosArrowDropright />
+          </div>
+          <div className="swiperNavPrev" ref={swiperNavPrevRef}>
+            <IoIosArrowDropleft />
+          </div>
+        </div>
+        {buttomData.map((val, key) => {
+          return (
+            <SwiperSlide key={key}>
+              <div className="items_flex">
                 <div
                   className="sl_img"
                   style={{ backgroundImage: `url(${val.Image})` }}
@@ -35,11 +74,11 @@ function Slider() {
                 <span className="sl_text">{val.title}</span>
                 <span className="sl_text2">{val.category}</span>
                 <span className="sl_price">{val.price}</span>
-              </motion.div>
-            );
-          })}
-        </motion.div>
-      </motion.div>
+              </div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </>
   );
 }
